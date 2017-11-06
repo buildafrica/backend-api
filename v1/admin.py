572083478b8c7791899875d28fad658fb6151636
EYITAYO\tayo OGUNBIYI <eyitayoogunbiyi@icloud.com>
2017-11-06 21:33:17 +0100

    'Updated'

--- v1/admin.py
+++ v1/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 
-from .models import UserProfile
+from .models import UserProfile,Case,Sighting
 
 admin.site.register(UserProfile)
+admin.site.register(Case)
+admin.site.register(Sighting)
