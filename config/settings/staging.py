a90495e0b288440efb969921464a497234f97b19
subomi <subomi.oluwalana@yahoo.com>
2018-08-30 17:10:27 +0100

    removed add-host bug

---
+++ config/settings/staging.py
@@ -0,0 +1,29 @@
+from .base import *
+
+
+DEBUG = True
+
+INTERNAL_IPS = [
+    '127.0.0.1',
+]
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ['POSTGRES_DB_NAME'],
+        'USER': os.environ['POSTGRES_DB_USER'],
+        'PASSWORD': os.environ['POSTGRES_USER_PASSWORD'],
+        'HOST': 'ec2-54-243-216-33.compute-1.amazonaws.com', 
+        'PORT': '5432',
+        }
+    }
+
+INSTALLED_APPS.append('debug_toolbar')
+
+MIDDLEWARE_CLASSES.append('debug_toolbar.middleware.DebugToolbarMiddleware', )
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
