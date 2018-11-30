953c677ad4c1c2689e3dc4068a78be4bda918913
subomi <subomi.oluwalana@yahoo.com>
2018-12-01 00:23:15 +0100

    fixed bug in environment

--- backend/environment.py
+++ backend/environment.py
@@ -3,5 +3,5 @@ import os
 ENVIRONMENT = os.environ['DCMPA_ENVIRONMENT']
 SETTINGS_MODULE = 'backend.settings.dev'
 
-if ENVIRONMENT == 'staging':
-    SETTINGS_MODULE = 'backend.settings.staging'
+if ENVIRONMENT == 'testing':
+    SETTINGS_MODULE = 'backend.settings.testing'
